9c65df5b7ac04ae9a2844c1c6b1198823182ac16
elen-oz <ozerova.lenka@gmail.com>
2024-05-26 11:59:49 +0200

    init: start project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// 1. A section motivating why you want to study this course. (Maximum 250 words)
+// 2. A section motivating why you think you would be a good future developer. (Maximum 250 words)
+// 3. A section containing a personal letter where you can introduce yourself to us. (Maximum 700 words)
+// 4. A section with a short description of what course or previous experience you have within front-end development. (Maximum 250 words)
+// 5. A section where you link to a public repository on your GitHub that demonstrates your skills within HTML, CSS, and JavaScript. In this section you must also explain your code and how you created your project. (Maximum 500 words)
+
+// For each section you must include a heading and at least one paragraph. You must also include at least one image on your website (however, no more than three images should be added).
+
+// Once you are done with your website, you must publish the website and include the link to the published/live website in your application. The link to your website should be pasted into your application in the section called “Personligt Brev”.
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default App;

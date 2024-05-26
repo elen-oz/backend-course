ed69c66ba75de2b10d4fed20d51417470455fc8e
elen-oz <ozerova.lenka@gmail.com>
2024-05-26 20:43:50 +0200

    fix: img display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
     <footer className='py-3 text-center'>
-      <a href='' className='text-neutral-500 hover:text-white p-2'>
+      <a
+        href='https://github.com/elen-oz/backend-course-assignment'
+        target='_blank'
+        className='text-neutral-500 hover:text-white p-2'
+      >
         this Project on Github
       </a>
     </footer>

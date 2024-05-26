050b0502808c3714858e8d124f32fd84b36b1c2f
elen-oz <ozerova.lenka@gmail.com>
2024-05-26 20:32:33 +0200

    feat: add footer & split app on components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='py-3 text-center'>
+      <a href='' className='text-neutral-500 hover:text-white p-2'>
+        this Project on Github
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import {
+  PiLinkedinLogoDuotone,
+  PiGithubLogoDuotone,
+  PiIdentificationCardDuotone,
+} from 'react-icons/pi';
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 w-full z-50'>
+      <div className='px-8 mx-auto max-w-7xl'>
+        <div className='w-full justify-between flex items-center'>
+          <div></div>
+
+          <nav className='flex items-baseline space-x-4'>
+            <a
+              className='border-b-2 border-neutral-500 hover:border-b-2 hover:border-white px-3 py-2 flex items-center gap-2'
+              href='https://elenoz.app/'
+              target='_blank'
+            >
+              <PiIdentificationCardDuotone size={25} />
+              Portfolio
+            </a>
+            <a
+              className='border-b-2 border-neutral-500 hover:border-b-2 hover:border-white px-3 py-2 flex items-center gap-2'
+              href='https://github.com/elen-oz'
+              target='_blank'
+            >
+              <PiGithubLogoDuotone size={25} />
+              GitHub
+            </a>
+            <a
+              className='border-b-2 border-neutral-500 hover:border-b-2 hover:border-white px-3 py-2 flex items-center gap-2'
+              href='https://www.linkedin.com/in/elen-oz/'
+              target='_blank'
+            >
+              <PiLinkedinLogoDuotone size={25} />
+              Linkedin
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

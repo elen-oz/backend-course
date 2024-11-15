68075b5487dc3cc17834127ed5fff91af7757698
elen-oz <ozerova.lenka@gmail.com>
2024-11-15 11:41:52 +0100

    feat: add link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,21 @@
 const Footer = () => {
   return (
-    <footer className='py-3 text-center'>
-      <a
-        href='https://github.com/elen-oz/backend-course/'
-        target='_blank'
-        className='text-neutral-500 hover:text-white p-2'
-      >
-        this Project on Github
-      </a>
-    </footer>
+      <footer className='py-3 text-center'>
+          <a
+              href='https://github.com/elen-oz/backend-course/'
+              target='_blank'
+              className='text-neutral-500 hover:text-white p-2'
+          >
+              This Project on Github
+          </a>
+          <a
+              href='https://backend-course-assignment.netlify.app/'
+              target='_blank'
+              className='text-neutral-500 hover:text-white p-2'
+          >
+              Live link to this Project
+          </a>
+      </footer>
   );
 };
 
